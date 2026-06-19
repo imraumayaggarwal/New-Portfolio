b770cee2249b6f83a3ede058704bc277d00df8c6
Raumay Aggarwal <aggarwalraumay@gmail.com>
2026-06-19 09:37:27 +0530

    feat: add navbar layout

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,7 +1,24 @@
+import Container from "../layout/Container";
+
 export default function Navbar() {
     return (
-        <div>
-            Navbar
-        </div>
+        <nav className="border-b">
+            <Container>
+                <div className="flex justify-between items-center h-16">
+                    <div>
+                        Raumay Aggarwal
+                    </div>
+                    <div className="flex gap-6">
+                        <a href="#about">About</a>
+                        <a href="#experience">Experience</a>
+                        <a href="#projects">Projects</a>
+                        <a href="#contact">Contact</a>
+                    </div>
+                    <div>
+                        Time
+                    </div>
+                </div>
+            </Container>
+        </nav>
     );
 }
