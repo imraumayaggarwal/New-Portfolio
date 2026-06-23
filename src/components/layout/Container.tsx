6d21c677bda8d11865720b082882abf33842b80e
Raumay Aggarwal <aggarwalraumay@gmail.com>
2026-06-23 20:07:22 +0530

    feat: update Container component to increase max width

--- src/components/layout/Container.tsx
+++ src/components/layout/Container.tsx
@@ -2,9 +2,10 @@ export default function Container({
     children
 }: {
     children : React.ReactNode;
+    className?: string;
 }) {
     return (
-        <div className="max-w-6xl px-4 mx-auto">
+        <div className="max-w-7xl px-4 mx-auto">
             {children}
         </div>
     );
